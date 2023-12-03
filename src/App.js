f8bd6e86f68de2d878492fcbca3b5355230e8a67
Jin <jin.chong@mrjinit.com>
2023-12-03 12:17:22 +0800

    React First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState }  from "react";
 
 function App() {
+
+  const initialState = [{id: 3317019, name: "Jin"},{id: 222, name: "Bob"},{id: 332, name: "Tim"}]; 
+
+  let dspUserList = "Hidden";
+
+  const [userList, setUserList] = useState(initialState);
+
+  const [userListVisability, setVisability] = useState(false);
+
+  function handleDelete(itemId) { //SELF NOTE: Why not manually use for loop/for each because complex and relies on legnth
+    setUserList(userList.filter(tmpUserElement => itemId !== tmpUserElement.id)) //Remember setUserList re-renders is triggered for ALL 
+  }
+
+  function handleVisability(visability) {
+    if(visability) {
+      setVisability(false);
+    } else {
+      setVisability(true);
+    }
+    
+  }
+
+  if(userListVisability) {
+    dspUserList = userList.map((userListItem) => ( 
+                  <li key="userListItem.id">
+                    <span>{userListItem.id} {userListItem.name}</span>
+                    <button onClick={() => handleDelete(userListItem.id)}>Delete</button>
+                  </li>
+                )) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <div>
+        <h1>User List</h1>
+        <ul>
+          <button onClick={() => handleVisability(userListVisability)}></button>
+          { //SELF NOTE: Why not handleDelete(...)? Executes straight away
+            dspUserList
+          } 
+        </ul>
+      </div>
     </div>
   );
+
 }
 
 export default App;
