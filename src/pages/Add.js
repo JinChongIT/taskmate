a6b495366c827ae3937868d842a801003c281f5e
Jin <jin.chong@mrjinit.com>
2023-12-13 14:27:21 +0800

    Implement onSubmit

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const Add = () => {
+
+  const frmTaskName = useRef();
+  const frmTaskNum = useRef();
+
+  function submitHandler(formData) {
+    formData.preventDefault();
+    console.log(frmTaskName.current.value);
+    formData.target.reset();
+    /*
+    const fd = new FormData(formData.target);
+    console.log(fd.get("txtTaskName"));
+    */
+  }
+
   return (
-    <div>Add</div>
+    <form onSubmit={submitHandler}>
+        <input type='text' name="txtTaskName" ref={frmTaskName} />
+        <input type='number' name="txtTaskNum" ref={frmTaskNum} />
+        <button type="submit">Add to Cart</button>
+    </form>
   )
 }
